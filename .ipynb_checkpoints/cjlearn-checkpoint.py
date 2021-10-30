f452dfb6d9e41ae541882b79d872a1de55767a86
Carl <jacob156@purdue.edu>
2021-10-30 17:29:32 -0400

    added exmaple notebook

---
+++ .ipynb_checkpoints/cjlearn-checkpoint.py
@@ -0,0 +1,75 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+import pandas as pd
+
+class random_line_maker:
+    def __init__(self,slope=1,intercept=0,variance=1,points=10):
+        """Instantiates a line with a given slope, intercept, variance, and number of points"""
+        self.slope = slope
+        self.intercept = intercept
+        self.variance = variance
+        self.points = points
+        self.x = self.__return_x(self.intercept,self.variance,self.slope,self.points)
+        self.y = self.__return_y(self.x,self.intercept,self.variance,self.slope,self.points)
+        
+    def __return_x(self,intercept,variance,slope,points):
+        x = np.arange(start=0,stop=points)
+        return x
+    
+    def __return_y(self,x,intercept,variance,slope,points):
+        def __residuals(variance=variance):
+            return [np.random.normal(scale=variance**2,loc=0) for i in range(points)] 
+        def f(x,intercept,slope):
+            return slope*x+intercept
+        y = f(x,intercept,slope)
+        y_with_variance = y + __residuals()
+        return y_with_variance
+    
+    def plot(self):
+        return plt.scatter(self.x,self.y)     
+    
+class models:
+    class slr:
+        def __init__(self):
+            self.x = None
+            self.y = None
+            self.beta1_hat = None
+            self.beta0_hat = None
+
+        def fit(self, line : random_line_maker = None, x = None,y = None):
+            '''Fits data via OLS simple linear regression'''
+
+            if (line == None) and (x == None or y == None):
+                raise Exception('Input either array like input for x and y or line object as arguments')
+            elif (line != None) and (x != None or y != None):
+                print('elif')
+                self.x = line.x
+                self.y = line.y
+            else:
+                self.x = line.x
+                self.y = line.y
+
+            numerator = 0
+            denominator = 0
+            x_bar = sum(self.x)/len(self.x)
+            y_bar = sum(self.y)/len(self.y)
+            n = len(self.x)
+            for i in range(n):
+                xi = self.x[i]
+                yi = self.y[i]
+                numerator+=(xi*yi)-(n*x_bar*y_bar)
+                denominator+=(xi**2)-n*(x_bar**2)
+
+            self.beta1_hat=numerator/denominator
+            self.beta0_hat = y_bar-self.beta1_hat*x_bar      
+
+        def predict(self,x):
+            return self.beta0_hat+self.beta1_hat*x
+
+        def plot(self):
+            fig, ax = plt.subplots()
+            ax.scatter(self.x, self.y, c='blue')
+            line = mlines.Line2D(xdata = self.x, ydata = self.predict(np.array(self.x)), color='red')
+            ax.add_line(line)
+            return plt  
